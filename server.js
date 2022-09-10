487996dcdd164aec5418c02edcf664ccde1b8a06
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-09-09 20:47:11 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,14 @@
+import express, { application } from "express";
+//import connectDatabase from './config/db';
+
+
+//initialize express application
+const app = express();
+
+//api endpoints
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+//connection listener
+app.listen(3000,() => console.log('Express server running on port 3000.'));
