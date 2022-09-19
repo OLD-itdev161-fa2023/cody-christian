e18fc6e9643554b2c6dc62e0b7678f1dae962969
Cody Christian <codychristian@Codys-Air.wi.rr.com>
2022-09-19 18:28:38 -0500

    Create User Registration API Endpoint

--- server.js
+++ server.js
@@ -8,10 +8,27 @@ const app = express();
 //connect database
 connectDatabase();
 
-//api endpoints
+//connect middleware
+app.use(express.json({ extended: false}));
+
+// API Endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
 
 //connection listener
 app.listen(3000,() => console.log('Express server running on port 3000.'));
