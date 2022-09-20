e6c2b1cbb06317cac58b0708fa5703e9d5a94628
Cody Christian <codychristian@Codys-Air.wi.rr.com>
2022-09-19 19:03:29 -0500

    Built Out User Registration API Endpoint

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express, { application } from 'express';
-import connectDatabase from './config/db';
-
+import express, { application } from "express";
+import connectDatabase from "./config/db";
+import { check, validationResult } from "express-validator";
 
 //initialize express application
 const app = express();
@@ -9,26 +9,34 @@ const app = express();
 connectDatabase();
 
 //connect middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 
 // API Endpoints
 /**
  * @route GET /
  * @desc Test endpoint
  */
-app.get('/', (req, res) =>
-    res.send('http get request sent to root api endpoint')
+app.get("/", (req, res) =>
+  res.send("http get request sent to root api endpoint")
 );
 
 /**
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
 
+app.post("/api/users", [
+  check("name", "Please Enter Your Name").not().isEmpty(),
+  check("email", "Please enter a valid email").not().isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+]);
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 //connection listener
-app.listen(3000,() => console.log('Express server running on port 3000.'));
+app.listen(3000, () => console.log("Express server running on port 3000."));
