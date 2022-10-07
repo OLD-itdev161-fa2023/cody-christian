0b53f5199815e0da727af4674c34cbc1769a9f3b
Cody Christian <codychristian@Codys-Air.attlocal.net>
2022-10-06 23:24:22 -0500

    Completed Activity 4

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-import express, { application } from "express";
-import connectDatabase from "./config/db";
-import { check, validationResult } from "express-validator";
+import express from 'express';
+import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+import cors from 'cors';
 
-//initialize express application
+// Initialize express application
 const app = express();
 
-//connect database
+// Connect database
 connectDatabase();
 
-//connect middleware
+// Configure Middleware
 app.use(express.json({ extended: false }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+);
 
-// API Endpoints
+// API endpoints
 /**
  * @route GET /
  * @desc Test endpoint
@@ -20,35 +26,34 @@ app.get('/', (req, res) =>
   res.send('http get request sent to root api endpoint')
 );
 
+app.get('/api/', (req, res) => res.send('http get request sent to api'));
+
 /**
  * @route POST api/users
  * @desc Register user
  */
-
 app.post(
-  '/api/users', 
+  '/api/users',
   [
-    check("name", "Please Enter Your Name").not().isEmpty(),
-    check("email", "Please enter a valid email").not().isEmail(),
+    check('name', 'Please enter your name')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
-  ).isLength({ min: 6 })
-],
-(req,res) => {
-  const errors= validationResult(req);
-  if (!errors.isEmpty()){
-    return res.status(422).json({errors: errors.array()});
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      return res.send(req.body);
+    }
   }
-  else {
-    return res.send(req.body);
-  }
-}
 );
-app.post("/api/users", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
-});
 
-//connection listener
-app.listen(3000, () => console.log("Express server running on port 3000."));
+// Connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
